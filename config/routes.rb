98d2c6e6abbc32b7788ddf084b0336f02bdc2902
Orest Kostiuk <xxmolekula@gmail.com>
2019-09-12 19:51:27 +0300

    added tolk

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount Tolk::Engine => '/tolk', :as => 'tolk'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users, controllers: { registrations: 'registrations' }

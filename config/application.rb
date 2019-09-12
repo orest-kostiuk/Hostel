971a4435012265fc20308827975a85c62336220e
Tanya Pushchinska <tanya.pushchinska.com>
2019-09-12 15:58:52 +0300

    Added locale for devise and active admin

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,8 @@ module Hostel
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
+    config.i18n.default_locale = :uk
+
     # Don't generate system test files.
     config.generators.system_tests = nil
   end

d072a189dc73b0f09726ecaf57eb7107cf2dd3f6
Orest Kostiuk <xxmolekula@gmail.com>
2019-09-20 11:23:55 +0300

    email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,5 +93,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: "http://localhost:3000" }
+  config.action_mailer.default_url_options = { host: "http://18.216.51.21" }
 end

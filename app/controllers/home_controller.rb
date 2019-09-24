87f52564556953c6b841a9106afd3a941f415cc0
Tanya Pushchinska <tanya.pushchinska.com>
2019-09-25 00:13:28 +0300

    added photo remove from active storage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,14 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
+
   def index
     @users = User.all
+  end
+
+  def delete_image_attachment
+    current_user.image = ActiveStorage::Blob.find_signed(params[:id])
+    current_user.image.purge
+    redirect_to edit_user_registration_url
   end
 end

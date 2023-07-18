86b9a1d5d1bb29e5a2c860fe83a42eb9e5635fa6
Orest Kostiuk <okostiuk@assurance.com>
2023-07-18 20:09:35 +0200

    added plugin registration ro initializer

---
+++ config/initializers/room_type_plugin.rb
@@ -0,0 +1,6 @@
+PLUGINS = {
+  'single_room_plugin' => Plugins::SingleRoomPlugin,
+  'double_room_plugin' => Plugins::DoubleRoomPlugin,
+  'triple_room_plugin.rb' => Plugins::TripleRoomPlugin,
+  'half_lux_plugin' => Plugins::HalfLuxPlugin
+}
